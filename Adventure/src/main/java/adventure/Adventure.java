5abf453315dbba53c75b1779044865207bf6c57a
polusy <p.marchisella@studenti.uniba.it>
2025-07-08 20:48:05 +0200

    aggiunta primo package

---
+++ Adventure/src/main/java/adventure/Adventure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adventure;
+
+/**
+ *
+ * @author Paolo
+ */
+public class Adventure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
