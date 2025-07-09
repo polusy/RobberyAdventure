4d02fb1ae1f3b803031a6f776d12e12df23a73e1
polusy <marchisella.p@gmail.com>
2025-07-09 15:16:02 +0200

    Definizione enum RoomId

---
+++ Adventure/src/main/java/adventure/identifiers/RoomId.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.identifiers;
+
+/**
+ *
+ * @author Paolo
+ */
+
+public enum RoomId {
+    
+	SIDEWALK,
+	MIDDLE_GARDEN,
+	EAST_GARDEN,
+	WEST_GARDEN,
+	STAIRCASE,
+	GARAGE,
+	BATHROOM,
+	LIVING_ROOM,
+	GYM,
+	LAUNDRY,
+	KITCHEN,
+	BEDROOM,
+	LIBRARY,
+	SECRET_ROOM,
+	ANTE_VAULT,
+	VAULT,
+	PAINTINGS_ROOM;
+}
