81156f750b9ebc4adb071ff3494a64b812149a26
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 17:56:25 +0200

    Definita la classe ServerManager

--- Adventure/src/main/java/adventure/Boundary/ServerManager.java
+++ Adventure/src/main/java/adventure/Boundary/ServerManager.java
@@ -4,10 +4,40 @@
  */
 package adventure.Boundary;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import java.net.URI;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import java.io.IOException;
+
+
 /**
  *
  * @author utente
  */
 public class ServerManager implements Runnable {
+    private String uri;
+    HttpServer genericServer;
+    
+    public ServerManager(String stringUri, int portNumber, Class<?> service){
+        URI uri = UriBuilder.fromUri(stringUri).port(portNumber).build();
+        ResourceConfig config = new ResourceConfig(service);
+        genericServer = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+    }
+
+    @Override
+    public void run()
+    {
+        try{
+            genericServer.start();
+        } catch (IOException exception){};
+    }
+
+    public HttpServer getServer()
+    {
+        return genericServer;
+    }
     
 }
