e4049053fd41a57984906da808136d10beef1179
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 13:35:04 +0200

    Definita la classe AdvObject con costruttori, getters e setters

---
+++ Adventure/src/main/java/adventure/Entity/objects/AdvObject.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.objects;
+
+import adventure.identifiers.ObjectId;
+import java.util.Set;
+
+/**
+ *
+ * @author utente
+ */
+public class AdvObject {
+    final ObjectId id;
+    final String name;
+    final String description;
+    final Set<String> alias;
+    boolean visible;
+
+    // Constructors
+    
+    public AdvObject(ObjectId id, String name, String description, Set<String> alias) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.alias = alias;
+    }
+
+    public AdvObject(ObjectId id, String name, String description, Set<String> alias, boolean visible) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.alias = alias;
+        this.visible = visible;
+    }
+
+    // Getter methods
+    
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<String> getAlias() {
+        return alias;
+    }
+
+    public boolean isVisible() { return visible; }
+    
+    // Setter method
+    public void setVisibility(boolean visible){
+        this.visible = visible;
+    }
+    
+}
