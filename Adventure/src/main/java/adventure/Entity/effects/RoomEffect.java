234668ae524d4903da130eecbbde1ba163d0f713
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 19:06:53 +0200

    Definiti final i due attributi della classe RoomEffect

--- Adventure/src/main/java/adventure/Entity/effects/RoomEffect.java
+++ Adventure/src/main/java/adventure/Entity/effects/RoomEffect.java
@@ -11,8 +11,8 @@ import adventure.identifiers.ObjectId;
  * @author utente
  */
 public class RoomEffect {
-    private ObjectId addingObject;
-    private ObjectId removingObject;
+    final private ObjectId addingObject;
+    final private ObjectId removingObject;
 
     public RoomEffect(ObjectId addingObject, ObjectId removingObject) {
         this.addingObject = addingObject;
