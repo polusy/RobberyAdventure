e1368301682fb0ef55a3c043fb83c4a4d061da05
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 12:55:30 +0200

    Definita la classe IdAlreadyTakenException

---
+++ Adventure/src/main/java/adventure/exceptions/EndGameException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.exceptions;
+
+/**
+ *
+ * @author utente
+ */
+public class EndGameException extends Exception {
+    private String message = "End game";
+    
+    public String getMessage() { return message; }    
+}
