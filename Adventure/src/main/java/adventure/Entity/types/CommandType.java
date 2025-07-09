77c16fc0de9a9f3f38bf88759e645d7b8050794c
polusy <marchisella.p@gmail.com>
2025-07-09 14:38:41 +0200

    Definizione enum CommandType

---
+++ Adventure/src/main/java/adventure/Entity/types/CommandType.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.types;
+
+/**
+ *
+ * @author Paolo
+ */
+public enum CommandType {
+    
+        END,
+        SAVE,
+        MENU,
+        INVENTORY,
+        LOOT_BAG,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        OPEN,
+        CLOSE,
+        PUSH,
+        BREAK,
+        PICK_UP,
+        DROP,
+        USE,
+        LOOK_AT,
+        CONSUME,
+        ACTIVATE,
+        DEACTIVATE,
+        MOVE,
+        FILL
+}
