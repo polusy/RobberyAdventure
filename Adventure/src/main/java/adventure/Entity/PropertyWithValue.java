f733bc0742fa71e775c3cae3b6c8ed2e8bc63e94
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 13:55:50 +0200

    Definita la classe PropertyWithValue

---
+++ Adventure/src/main/java/adventure/Entity/PropertyWithValue.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity;
+
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public abstract class PropertyWithValue extends Property {
+    private boolean value;
+    final private PropertyType propertyType; 
+
+    // Constructors
+    
+    public PropertyWithValue(PropertyType propertyType) {
+        this.propertyType = propertyType;
+    }
+
+    // Getter methods
+    
+    public boolean getValue() {
+        return value;
+    }
+
+    public PropertyType getType(){ return propertyType; }
+    
+    // Setter method
+    
+    public void setValue(boolean value) {
+        this.value = value;
+    }    
+}
