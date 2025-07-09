805e692bfe6852db57a2c0f2594f261dcd3c7651
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 18:10:15 +0200

    Definita la classe Consumable

---
+++ Adventure/src/main/java/adventure/Entity/properties/Consumable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.properties;
+
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public class Consumable extends PropertyWithValue {
+    
+    public Consumable(boolean value){
+        super(PropertyType.CONSUMABLE);
+	super.setValue(value);
+    }    
+}
