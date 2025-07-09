2a699e0e1b519ab40d5928d39dd07315ab778539
polusy <marchisella.p@gmail.com>
2025-07-09 15:57:25 +0200

    definizione classe LootBagEffect

---
+++ Adventure/src/main/java/adventure/Entity/effects/LootBagEffect.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.effects;
+
+import adventure.identifiers.ObjectId;
+
+/**
+ *
+ * @author Paolo
+ */
+public class LootBagEffect {
+
+    private ObjectId addingObject;
+
+    public LootBagEffect(ObjectId addingObject) {
+        this.addingObject = addingObject;
+    }
+
+    public ObjectId getAddingObject() {
+        return addingObject;
+    }
+
+    public void setAddingObject(ObjectId addingObject) {
+        this.addingObject = addingObject;
+    }
+    
+}
