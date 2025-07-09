7529ef51fbbf23189c82e811e960e97b29ae9201
polusy <marchisella.p@gmail.com>
2025-07-09 14:48:31 +0200

    Definizione classe Preposition

---
+++ Adventure/src/main/java/adventure/utilities/Preposition.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.utilities;
+
+/**
+ *
+ * @author Paolo
+ */
+public class Preposition {
+    
+    private String preposition;
+    
+    public void Preposition(String preposition){
+        this.preposition = preposition;
+    }
+
+    public String getPreposition() {
+        return preposition;
+    }
+
+    public void setPreposition(String preposition) {
+        this.preposition = preposition;
+    }
+    
+    
+}
