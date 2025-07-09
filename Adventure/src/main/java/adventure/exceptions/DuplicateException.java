2791faca45a1c3d8d55f76302823b1c52dc15820
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 12:49:34 +0200

    Definita la classe DuplicateException

---
+++ Adventure/src/main/java/adventure/exceptions/DuplicateException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.exceptions;
+
+/**
+ *
+ * @author utente
+ */
+public class DuplicateException extends Exception {
+    private String message = "Duplicate element";
+    
+    public String getMessage() { return message; } 
+}
