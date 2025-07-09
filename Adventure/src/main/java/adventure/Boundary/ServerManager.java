970cd03048903cbc209e24174d6b416e09463c92
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-09 16:34:22 +0200

    Definita la classe ServerManager nel pacchetto adventure.Boundary

--- Adventure/src/main/java/adventure/Boundary/ServerManager.java
+++ Adventure/src/main/java/adventure/Boundary/ServerManager.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package adventure.Entity;
+package adventure.Boundary;
 
 /**
  *
  * @author utente
  */
-public abstract class Property {
+public class ServerManager implements Runnable {
     
 }
