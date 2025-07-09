50b50e01c6348d2d1de2634f7d7b210f2df95d16
polusy <marchisella.p@gmail.com>
2025-07-09 16:57:03 +0200

    Definizione classe Fillable

---
+++ Adventure/src/main/java/adventure/Entity/properties/Fillable.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.properties;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author Paolo
+ */
+public class Fillable extends PropertyWithValue {
+    
+    public Fillable(boolean value){
+        super(PropertyType.FILLABLE);
+        super.setValue(value);
+    }
+    
+}
