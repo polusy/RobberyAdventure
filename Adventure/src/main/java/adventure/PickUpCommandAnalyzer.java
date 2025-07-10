4d16470b28ffd603506ca139ec7278f98dca8301
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-10 19:49:57 +0200

    Definita la classe PickUpCommandAnalyzer

---
+++ Adventure/src/main/java/adventure/PickUpCommandAnalyzer.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public class PickUpCommandAnalyzer extends CommandAnalyzer{
+    public PickUpCommandAnalyzer(){
+        super(" Pensi veramente di riuscire a mettere questa cosa nello zaino!? ");
+    }
+    
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput) throws NotValidSentenceException
+    {
+	return CommandAnalyzer.analyzeUnaryCommand(parserOutput, PropertyType.PICKUPABLE, errorMessage);
+    }
+     
+}
