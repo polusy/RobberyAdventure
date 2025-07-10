9574b33dbdd064a7ed2c3f3f8630d05c8a97eea9
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-10 19:34:56 +0200

    Definita la classe PushCommandAnalyzer

---
+++ Adventure/src/main/java/adventure/PushCommandAnalyzer.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public class PushCommandAnalyzer extends CommandAnalyzer{
+    public PushCommandAnalyzer(){
+        super("Ma ti rendi conto che stai cercando di premere questo oggetto?!");
+    }
+    
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput) throws NotValidSentenceException, AmbiguousCommandException
+    {
+	return CommandAnalyzer.analyzeUnaryCommand(parserOutput, PropertyType.PUSHABLE, errorMessage);
+    }
+    
+}
