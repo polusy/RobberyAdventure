87e67b4d47910e7501c6381f1ca4f41bad035660
polusy <marchisella.p@gmail.com>
2025-07-10 21:30:19 +0200

    Definizione della classe InventoryCommandObserver

---
+++ Adventure/src/main/java/adventure/Control/observers/InventoryCommandObserver.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Control.observers;
+
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.NotValidSentenceException;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author Paolo
+ */
+public class InventoryCommandObserver implements GameObserver{
+    
+    public void update(GameDescription gameDescription, ParserOutput parserOutput, PrintStream out) throws NotValidSentenceException
+    {
+	if (parserOutput.getDoorRoom() != null || parserOutput.getObjects() != null)
+		throw new NotValidSentenceException();
+
+
+	StringBuilder inventoryItemsString = new StringBuilder("Il tuo inventario:%n");
+	
+	List<String> objectNames = gameDescription.getInventory().getObjects().stream()
+		.map(object -> object.getName())
+		.collect(Collectors.toList());
+
+	for (String objectName : objectNames){
+		out.println("- " + objectName + "%n");
+	}
+
+	out.println("%n");
+
+    }
+    
+}
