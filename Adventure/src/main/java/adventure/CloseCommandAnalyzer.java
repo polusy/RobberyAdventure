aef740f157ebe86cdad0710037bbe44a706dd1c2
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-10 19:04:02 +0200

    Definita la classe CloseCommandAnalyzer

---
+++ Adventure/src/main/java/adventure/CloseCommandAnalyzer.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public class CloseCommandAnalyzer extends CommandAnalyzer{
+    public CloseCommandAnalyzer(){
+        super("Non puoi chiudere questo oggetto!");
+    }
+    
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput) throws NotValidSentenceException, AmbiguousCommandException
+    {
+	return CommandAnalyzer.analyzeUnaryCommand(parserOutput, PropertyType.OPENABLE, errorMessage);
+    }
+
+}
