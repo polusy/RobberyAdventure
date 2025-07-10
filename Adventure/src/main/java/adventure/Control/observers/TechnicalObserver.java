86db95ce28a2ef24f6c17ae5eac9d70262e55860
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-10 19:42:27 +0200

    Spostata l'interfaccia TechnicalObserver nel pacchetto adventure.Control.observers

--- Adventure/src/main/java/adventure/Control/observers/TechnicalObserver.java
+++ Adventure/src/main/java/adventure/Control/observers/TechnicalObserver.java
@@ -2,10 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package adventure;
+package adventure.Control.observers;
 
 import adventure.exceptions.NotValidSentenceException;
 import adventure.exceptions.EndGameException;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
 
 /**
  *
@@ -13,5 +15,5 @@ import adventure.exceptions.EndGameException;
  */
 public interface TechnicalObserver {
     public void update (GameDescription game, ParserOutput parserOutput, StringBuilder message) 
-            throws NotValidSentenceException, EndGameException ;
+        throws NotValidSentenceException, EndGameException ;
 }
