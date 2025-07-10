bd6bab60750a1a51d576617e1b162a7fb6bdeb28
polusy <marchisella.p@gmail.com>
2025-07-10 22:24:04 +0200

    Definita classe ActivateCommandAnalyzer

---
+++ Adventure/src/main/java/adventure/Control/analyzers/ActivateCommandAnalyzer.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Control.analyzers;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.AmbiguousCommandException;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author Paolo
+ */
+public class ActivateCommandAnalyzer extends CommandAnalyzer{
+    
+        public ActivateCommandAnalyzer(){
+        super("Mi sembra impossibile attivare questo oggetto!");
+    }
+    
+    @Override
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput) throws NotValidSentenceException, AmbiguousCommandException
+    {
+	return analyzeUnaryCommand(parserOutput, PropertyType.ACTIVATABLE, errorMessage);
+    }
+    
+    
+}
