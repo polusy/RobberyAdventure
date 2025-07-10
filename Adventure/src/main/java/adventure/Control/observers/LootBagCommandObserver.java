bbc3ea84f161db76c6ab22e2cedad412e7dbc6c3
polusy <marchisella.p@gmail.com>
2025-07-10 22:03:47 +0200

    Definizione classe LootBagCommandObserver

---
+++ Adventure/src/main/java/adventure/Control/observers/LootBagCommandObserver.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Control.observers;
+
+
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.Entity.types.RobberyAdventure;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.Entity.objects.ValuableObject;
+import java.util.stream.Collectors;
+import java.util.List;
+import java.io.PrintStream;
+/**
+ *
+ * @author Paolo
+ */
+public class LootBagCommandObserver implements GameObserver{
+    
+    @Override
+    public void update(GameDescription gameDescription, ParserOutput parserOutput, PrintStream out) throws NotValidSentenceException{
+        StringBuilder message = new StringBuilder("La tua refurtiva si compone di: ").append(System.lineSeparator());
+	if (parserOutput.getDoorRoom() == null && parserOutput.getObjects() == null)
+	{
+            if (gameDescription instanceof RobberyAdventure)
+            {
+                RobberyAdventure robberyAdventure = (RobberyAdventure) gameDescription;
+		for (ValuableObject object : robberyAdventure.getLootBag().getObjects())
+		{
+                   message.append(object.getName()).append(", valore : ").append(object.getValue()).append(System.lineSeparator());
+		}
+
+		message.append("Totale rubato : " + robberyAdventure.getLootBag().getTotalStolenAmount());
+		out.println(message);
+            }
+	}
+	else{
+		throw new NotValidSentenceException();
+	}
+    }
+    
+}
