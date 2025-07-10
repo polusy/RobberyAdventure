86615dca90183906c57bba0cd504d25c35160f55
polusy <marchisella.p@gmail.com>
2025-07-10 22:03:31 +0200

    Definizione classe RobberyAdventure con attributo necessario

---
+++ Adventure/src/main/java/adventure/Entity/types/RobberyAdventure.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Entity.types;
+
+/**
+ *
+ * @author Paolo
+ */
+public class RobberyAdventure extends GameDescription{
+    
+    private LootBag lootBag;
+
+    public LootBag getLootBag() {
+        return lootBag;
+    }
+    
+    
+    
+    @Override
+    public void init(){};
+    
+    
+    
+}
