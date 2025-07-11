8a46ae22bb3208324d62c853872b3cf3b45ff040
polusy <marchisella.p@gmail.com>
2025-07-11 10:53:58 +0200

    Definizione classe DeactivateCommandaAnalyzer

---
+++ Adventure/src/main/java/adventure/Control/analyzers/DeactivateCommandAnalyzer.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Control.analyzers;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author Paolo
+ */
+public class DeactivateCommandAnalyzer extends CommandAnalyzer{
+        public DeactivateCommandAnalyzer(){
+        super(" Non puoi disattivare un oggetto del genere...");
+    }
+    
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput) throws NotValidSentenceException
+    {
+	return analyzeUnaryCommand(parserOutput, PropertyType.ACTIVATABLE, errorMessage);
+    }
+    
+    
+    
+}
