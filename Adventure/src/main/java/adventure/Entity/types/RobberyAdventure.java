ae08e58cc56a7b031c66ea44cb0b3efa22cd248d
polusy <marchisella.p@gmail.com>
2025-07-11 18:59:30 +0200

    Inseriti i catch nel blocco initRooms

--- Adventure/src/main/java/adventure/Entity/types/RobberyAdventure.java
+++ Adventure/src/main/java/adventure/Entity/types/RobberyAdventure.java
@@ -7,8 +7,15 @@ package adventure.Entity.types;
 import adventure.Entity.objects.AdvObject;
 import adventure.Entity.objects.ValuableObject;
 import adventure.identifiers.CommandType;
+import adventure.utilities.Utils;
+import adventure.identifiers.RoomId;
 import adventure.exceptions.*;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 /**
  *
  * @author Paolo
@@ -38,6 +45,7 @@ public class RobberyAdventure extends GameDescription{
     public void init(){
         
         initCommands();
+        initRooms();
     
     
     
@@ -68,6 +76,34 @@ public class RobberyAdventure extends GameDescription{
 	addCommand(CommandType.LOOK_AT, "osserva", new String[]{"osservare", "guardare", "guarda", "vedere", "vedi", "descrivere", 	"descrivi"});
 
 	}
+    
+    
+    private void initRooms(){
+        
+        try{
+            addRoom(new Room(RoomId.ANTE_VAULT, "ante vault", Utils.loadFileDescriptionInString(new File("./resources/descriptions/ante_vault"))));
+            addRoom(new Room(RoomId.BATHROOM, "bagno", Utils.loadFileDescriptionInString(new File("./resources/descriptions/bathroom"))));     
+            addRoom(new Room(RoomId.BEDROOM, "camera da letto", Utils.loadFileDescriptionInString(new File("./resources/descriptions/bedroom"))));     
+            addRoom(new Room(RoomId.EAST_GARDEN, "giardino est", Utils.loadFileDescriptionInString(new File("./resources/descriptions/east_garden"))));
+            addRoom(new Room(RoomId.GARAGE, "garage", Utils.loadFileDescriptionInString(new File("./resources/descriptions/garage"))));     
+            addRoom(new Room(RoomId.GYM, "palestra", Utils.loadFileDescriptionInString(new File("./resources/descriptions/gym"))));   
+            addRoom(new Room(RoomId.KITCHEN, "cucina", Utils.loadFileDescriptionInString(new File("./resources/descriptions/kitchen"))));
+            addRoom(new Room(RoomId.LAUNDRY, "lavanderia", Utils.loadFileDescriptionInString(new File("./resources/descriptions/laundry"))));     
+            addRoom(new Room(RoomId.LIBRARY, "biblioteca", Utils.loadFileDescriptionInString(new File("./resources/descriptions/library"))));   
+            addRoom(new Room(RoomId.LIVING_ROOM, "salone", Utils.loadFileDescriptionInString(new File("./resources/descriptions/living_room"))));
+            addRoom(new Room(RoomId.MIDDLE_GARDEN, "giardino centrale", Utils.loadFileDescriptionInString(new File("./resources/descriptions/middle_garden"))));     
+            addRoom(new Room(RoomId.PAINTINGS_ROOM, "stanza dei quadri", Utils.loadFileDescriptionInString(new File("./resources/descriptions/paintings_room"))));   
+            addRoom(new Room(RoomId.SECRET_ROOM, "stanza segreta", Utils.loadFileDescriptionInString(new File("./resources/descriptions/secret_room"))));
+            addRoom(new Room(RoomId.SIDEWALK, "marciapiede", Utils.loadFileDescriptionInString(new File("./resources/descriptions/sidewalk"))));     
+            addRoom(new Room(RoomId.STAIRCASE, "scalinata", Utils.loadFileDescriptionInString(new File("./resources/descriptions/staircase"))));   
+            addRoom(new Room(RoomId.VAULT, "vault", Utils.loadFileDescriptionInString(new File("./resources/descriptions/vault"))));
+            addRoom(new Room(RoomId.WEST_GARDEN, "giardino ovest", Utils.loadFileDescriptionInString(new File("./resources/descriptions/west_garden"))));  
+            
+        }catch(DuplicateException exception){}
+        catch(IOException exception){};
+        
+        
+    }
         
   
     
@@ -79,6 +115,14 @@ public class RobberyAdventure extends GameDescription{
         Command command = new Command(commandType, name, alias);
         this.getCommands().add(command);
         
+    }
+    
+    private void addRoom(Room room)throws DuplicateException{
+        if (this.getRooms().contains(room))
+            throw new DuplicateException();
+        
+        this.getRooms().add(room);
+           
     }
     
     
