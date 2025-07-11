e944df30d627ca9efa2f2950b9376c5fa2c18e88
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-11 11:34:55 +0200

    Definita la classe ConsumeCommandAnalyzer

---
+++ Adventure/src/main/java/adventure/Control/analyzers/ConsumeCommandAnalyzer.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adventure.Control.analyzers;
+
+import adventure.Entity.types.CommandAnalysisResult;
+import adventure.Entity.types.GameDescription;
+import adventure.Entity.types.ParserOutput;
+import adventure.exceptions.NotValidSentenceException;
+import adventure.exceptions.AmbiguousCommandException;
+import adventure.identifiers.PropertyType;
+
+/**
+ *
+ * @author utente
+ */
+public class ConsumeCommandAnalyzer extends CommandAnalyzer {
+    public ConsumeCommandAnalyzer(){
+        super(" Non puoi in nessun modo utilizzare questa cosa! ");
+    }
+    
+    @Override
+    public CommandAnalysisResult analyze(GameDescription gameDescription, ParserOutput parserOutput)
+            throws NotValidSentenceException, AmbiguousCommandException     {
+	return analyzeUnaryCommand(parserOutput, PropertyType.CONSUMABLE, errorMessage);
+    }
+}
