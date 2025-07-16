1d14188b3386ae7d4ad86c72f6a3bd3c7ec0a78e
BlackFlagWithAGoldenN <alepiccolo022@gmail.com>
2025-07-16 16:54:49 +0200

    Inserite le annotazioni javadoc per la classe PassingConditionResult

--- Adventure/src/main/java/adventure/Entity/types/PassingConditionResult.java
+++ Adventure/src/main/java/adventure/Entity/types/PassingConditionResult.java
@@ -6,7 +6,14 @@ package adventure.Entity.types;
 
 import adventure.Entity.effects.GameEffect;
 
-/**
+/** Risultato della valutazione della condizione completa rappresentata da un oggetto {@link CompleteCondition} di una specifica
+ * di un'azione sul gioco, nel caso in cui tale condizione sia soddisfatta rispetto alla sessione di gioco in corso.
+ * 
+ * La classe conserva un oggetto {@link GameEffect} che rappresenta l'effetto da applicare alla partita in corso, in seguito
+ * alla valutazione con successo della {@link CompleteCondition} relativa alla specifica della corrispondente azione sul gioco.
+ * 
+ * La classe conserva inoltre il messaggio che notifica l'applicaazione dell'azione sul gioco, di cui lo specifico oggetto
+ * GameEffect fa parte
  *
  * @author Paolo
  */
@@ -17,8 +24,9 @@ public class PassingConditionResult {
 
     /**
      *
-     * @param gameEffect
-     * @param passingConditionMessage
+     * @param gameEffect Effetto da applicare alla partita in corso, in seguito alla valutazione con successo della {@link CompleteCondition} corrispondente
+     * rispetto alla sessione di gioco in corso
+     * @param passingConditionMessage Messaggio che notifica l'applicazione del {@link GameEffect} sul gioco
      */
     public PassingConditionResult(GameEffect gameEffect, String passingConditionMessage) {
         this.gameEffect = gameEffect;
